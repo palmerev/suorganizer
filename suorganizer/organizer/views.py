fcb56119457cd75296e490cfb70e46b24686215f
Evan Palmer <evjpalmer@gmail.com>
2016-04-12 13:41:39 -0700

    update homepage view to render template

--- suorganizer/organizer/views.py
+++ suorganizer/organizer/views.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponse
+from django.template import Context, loader
 
 from .models import Tag
 
 
 def homepage(request):
     tag_list = Tag.objects.all()
-    output = ", ".join([tag.name for tag in tag_list])
-    return HttpResponse(output)
+    template = loader.get_template('organizer/tag_list.html')
+    context = Context({'tag_list': tag_list})
+    return HttpResponse(template.render(context))
