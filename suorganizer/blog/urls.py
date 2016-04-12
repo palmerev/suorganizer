a34edf89f568d43d40d977a89f23dfa8e51ca35c
Evan Palmer <evjpalmer@gmail.com>
2016-04-12 15:55:24 -0700

    refactor blog/post_list view to Class-based view

--- suorganizer/blog/urls.py
+++ suorganizer/blog/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url
 
-from .views import post_list, post_detail
+from .views import PostList, post_detail
 
 urlpatterns = [
     url(r'^(?P<year>\d{4})/'
         r'^(?P<month>\d{1,2})/'
         r'^(?P<slug>[\w\-]+)/$',
         post_detail, name="blog_post_detail"),
-    url(r'^$', post_list, name="blog_post_list"),
+    url(r'^$', PostList.as_view(), name="blog_post_list"),
 ]
--- suorganizer/blog/views.py
+++ suorganizer/blog/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import View
 
 from .models import Post
 
 
-def post_list(request):
-    post_list = Post.objects.all()
-    return render(request, 'blog/post_list.html', {'post_list': post_list})
+class PostList(View):
+    def get(self, request):
+        post_list = Post.objects.all()
+        return render(
+            request, 'blog/post_list.html', {'post_list': post_list})
 
 
 def post_detail(request, year, month, slug):
