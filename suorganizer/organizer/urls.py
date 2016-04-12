c5ecccb12919be6f6469d4fc8b8e0eabca5e77e4
Evan Palmer <evjpalmer@gmail.com>
2016-04-12 14:11:38 -0700

    create separate urls.py file for organizer app

---
+++ suorganizer/organizer/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+
+from .views import homepage, tag_detail
+
+urlpatterns = [
+    url(r'^$', homepage),
+    url(r'^tag/(?P<slug>[\w\-]+)/$', tag_detail, name='organizer_tag_detail'),
+]
--- suorganizer/suorganizer/urls.py
+++ suorganizer/suorganizer/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from organizer.views import homepage, tag_detail
+from organizer import urls as organizer_urls
 
 urlpatterns = [
     # Examples:
@@ -9,6 +9,5 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', homepage),
-    url(r'^tag/(?P<slug>[\w\-]+)/$', tag_detail, name='organizer_tag_detail'),
+    url(r'^', include(organizer_urls))
 ]
