58142852b394def852629a4140788f5d50baeb03
Evan Palmer <evjpalmer@gmail.com>
2016-04-12 14:03:06 -0700

    refactor views to use render shortcut instead of render_to_response

--- suorganizer/organizer/views.py
+++ suorganizer/organizer/views.py
@@ -1,15 +1,13 @@
-from django.shortcuts import get_object_or_404, render_to_response
+from django.shortcuts import get_object_or_404, render
 
 from .models import Tag
 
 
 def homepage(request):
-    return render_to_response(
-        'organizer/tag_list.html', {'tag_list': Tag.objects.all()})
+    return render(request, 'organizer/tag_list.html',
+        {'tag_list': Tag.objects.all()})
 
 
 def tag_detail(request, slug):
     tag = get_object_or_404(Tag, slug__iexact=slug)
-    return render_to_response(
-        'organizer/tag_detail.html',
-        {'tag': tag})
+    return render(request, 'organizer/tag_detail.html', {'tag': tag})
