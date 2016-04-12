0ddc618a5fec16a3a30055eb619fa07671af40e8
Evan Palmer <evjpalmer@gmail.com>
2016-04-12 11:45:42 -0700

    wire up basic homepage view/URL

--- suorganizer/suorganizer/urls.py
+++ suorganizer/suorganizer/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from organizer.views import homepage
+
 urlpatterns = [
     # Examples:
     # url(r'^$', 'suorganizer.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', homepage),
 ]
