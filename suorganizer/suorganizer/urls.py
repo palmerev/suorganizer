4b8f2284b03ed3a6f1c7fc69913cfbf23aa2c8f7
Evan Palmer <evjpalmer@gmail.com>
2016-04-12 13:58:00 -0700

    add URL for tag_detail

--- suorganizer/suorganizer/urls.py
+++ suorganizer/suorganizer/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from organizer.views import homepage
+from organizer.views import homepage, tag_detail
 
 urlpatterns = [
     # Examples:
@@ -10,4 +10,5 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', homepage),
+    url(r'^tag/(?P<slug>[\w\-]+)/$', tag_detail, name='organizer_tag_detail'),
 ]
