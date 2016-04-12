3118dbc1fb97050add27714a5976f52dd455b724
Evan Palmer <evjpalmer@gmail.com>
2016-04-12 13:59:41 -0700

    refactor views to use django shortcuts

--- suorganizer/organizer/views.py
+++ suorganizer/organizer/views.py
@@ -1,11 +1,15 @@
-from django.http import HttpResponse
-from django.template import Context, loader
+from django.shortcuts import get_object_or_404, render_to_response
 
 from .models import Tag
 
 
 def homepage(request):
-    tag_list = Tag.objects.all()
-    template = loader.get_template('organizer/tag_list.html')
-    context = Context({'tag_list': tag_list})
-    return HttpResponse(template.render(context))
+    return render_to_response(
+        'organizer/tag_list.html', {'tag_list': Tag.objects.all()})
+
+
+def tag_detail(request, slug):
+    tag = get_object_or_404(Tag, slug__iexact=slug)
+    return render_to_response(
+        'organizer/tag_detail.html',
+        {'tag': tag})
