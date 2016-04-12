abc327f1fc5c0804b7fbc7a21adf0c980bc9d342
Evan Palmer <evjpalmer@gmail.com>
2016-04-12 14:26:50 -0700

    add startup_list and startup_detail views and urls

--- suorganizer/organizer/urls.py
+++ suorganizer/organizer/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import url
 
-from .views import homepage, tag_detail
+from .views import (tag_list, tag_detail, startup_list, startup_detail)
 
 urlpatterns = [
-    url(r'^$', homepage),
+    url(r'^tag/$', tag_list, name='organizer_tag_list'),
     url(r'^tag/(?P<slug>[\w\-]+)/$', tag_detail, name='organizer_tag_detail'),
+    url(r'^startup/$',
+        startup_list, name='organizer_startup_list'),
+    url(r'^startup/(?P<slug>[\w\-]+)/$',
+        startup_detail, name='organizer_startup_detail'),
 ]
--- suorganizer/organizer/views.py
+++ suorganizer/organizer/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import get_object_or_404, render
 
-from .models import Tag
+from .models import Tag, Startup
 
 
-def homepage(request):
+def tag_list(request):
     return render(request, 'organizer/tag_list.html',
         {'tag_list': Tag.objects.all()})
 
@@ -11,3 +11,14 @@ def homepage(request):
 def tag_detail(request, slug):
     tag = get_object_or_404(Tag, slug__iexact=slug)
     return render(request, 'organizer/tag_detail.html', {'tag': tag})
+
+
+def startup_list(request):
+    return render(request, 'organizer/startup_list.html',
+        {'startup_list': Startup.objects.all()})
+
+
+def startup_detail(request, slug):
+    startup = get_object_or_404(Startup, slug__iexact=slug)
+    return render(request, 'organizer/startup_detail.html',
+    {'startup': startup})
