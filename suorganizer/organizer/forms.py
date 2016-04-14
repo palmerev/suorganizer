8fb2a0163b7068cd444cedb0ce20be6800045a87
Evan Palmer <evjpalmer@gmail.com>
2016-04-13 17:33:20 -0700

    add TagForm ModelForm in organizer/forms.py

---
+++ suorganizer/organizer/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from django.core.exceptions import ValidationError
+
+from .models import Tag
+
+
+class TagForm(forms.ModelForm):
+    class Meta:
+        model = Tag
+        fields = '__all__'
+
+    def clean_name(self):
+        return self.cleaned_data['name'].lower()
+
+    def clean_slug(self):
+        new_slug = self.cleaned_data['slug'].lower()
+        if new_slug == 'create':
+            raise ValidationError('Slug may not be "create".')
+        return new_slug
