a1026ef8b0cf2d3e0b7c88058724f87a993bb479
Evan Palmer <evjpalmer@gmail.com>
2016-04-13 17:53:36 -0700

    add blog PostForm and other organizer forms, put clean_slug into mixin

--- suorganizer/organizer/forms.py
+++ suorganizer/organizer/forms.py
@@ -1,10 +1,20 @@
 from django import forms
 from django.core.exceptions import ValidationError
 
-from .models import Tag
+from .models import Tag, NewsLink, Startup
 
 
-class TagForm(forms.ModelForm):
+class SlugCleanMixin:
+    """Mixin class providing a common slug cleaning method."""
+
+    def clean_slug(self):
+        new_slug = self.cleaned_data['slug'].lower()
+        if new_slug == 'create':
+            raise ValidationError('Slug may not be "create".')
+            return new_slug
+
+
+class TagForm(SlugCleanMixin, forms.ModelForm):
     class Meta:
         model = Tag
         fields = '__all__'
@@ -12,8 +22,14 @@ class TagForm(forms.ModelForm):
     def clean_name(self):
         return self.cleaned_data['name'].lower()
 
-    def clean_slug(self):
-        new_slug = self.cleaned_data['slug'].lower()
-        if new_slug == 'create':
-            raise ValidationError('Slug may not be "create".')
-        return new_slug
+
+class StartupForm(SlugCleanMixin, forms.ModelForm):
+    class Meta:
+        model = Startup
+        fields = '__all__'
+
+
+class NewsLinkForm(forms.ModelForm):
+    class Meta:
+        model = NewsLink
+        fields = '__all__'
